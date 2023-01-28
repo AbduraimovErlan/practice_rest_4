751bf3c223deefc860781bf4b09fef6cff081ca1
AbduraimovErlan <96403251+AbduraimovErlan@users.noreply.github.com>
2023-01-28 18:44:33 +0600

    commit_3

--- dfr/urls.py
+++ dfr/urls.py
@@ -5,8 +5,8 @@ from rest_framework.urlpatterns import format_suffix_patterns
 
 
 urlpatterns = [
-    url(r'^snippets/$', views.snipper_list),
-    url(r'^snippets/(?P<pk>[0-9]+)/$', views.snippet_derail),
+    url(r'^snippets/$', views.SnipperList.as_view()),
+    url(r'^snippets/(?P<pk>[0-9]+)/$', views.SnippetDetail.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
